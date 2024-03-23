c1d46cf1042a935b8542ed53d30b22875a5a0aec
Alii-s <119129254+Alii-s@users.noreply.github.com>
2024-03-23 23:14:22 +0200

    Full code except small issue

--- ImageUploaderAPI/Program.cs
+++ ImageUploaderAPI/Program.cs
@@ -1,6 +1,107 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using imageUploaderAPI;
+using System.Text.Json;
+using System.Xml;
 
-app.MapGet("/", () => "Hello World!");
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddControllers();
+builder.Services.AddSwaggerGen();
+var app = builder.Build();
+app.UseDefaultFiles();
+app.UseStaticFiles();
+// Configure the HTTP request pipeline.
+
+app.MapPost("/api", async (HttpContext context) =>
+{
+    var form = await context.Request.ReadFormAsync();
+    var file = form.Files.GetFile("img");
+    if (string.IsNullOrWhiteSpace(form["name"]) || form.Files["img"]==null)
+    {
+        context.Response.StatusCode = StatusCodes.Status400BadRequest;
+        await context.Response.WriteAsync("Name and Image are required.");
+        return;
+    }
+
+    if (file != null)
+    {
+        var allowedExtensions = new[] { ".png", ".gif", ".jpeg" };
+        var fileExtension = Path.GetExtension(file.FileName).ToLowerInvariant();
+        if (!allowedExtensions.Contains(fileExtension))
+        {
+            context.Response.StatusCode = StatusCodes.Status400BadRequest;
+            await context.Response.WriteAsync("Only PNG, GIF, and JPEG images are allowed.");
+            return;
+        }
+    }
+
+    var data = new pictureData
+    {
+        Name = form["name"],
+        Image = file.FileName,
+        id = Guid.NewGuid().ToString()
+    };
+    var json = JsonSerializer.Serialize(data);
+    var filePath = Path.Combine(app.Environment.ContentRootPath,"images.json");
+    var imageFilePath = Path.Combine(app.Environment.ContentRootPath, "data", $"{data.id}{Path.GetExtension(data.Image)}");
+    using (var stream = new FileStream(imageFilePath, FileMode.Create))
+    {
+        await file.CopyToAsync(stream);
+    }
+    await File.WriteAllTextAsync(filePath, json);
+    context.Response.StatusCode = StatusCodes.Status200OK;
+    await context.Response.WriteAsync(data.id);
+
+});
+
+
+app.MapGet("/picture/{id}", async (HttpContext context) =>
+{
+
+    var uniqueId = context.Request.RouteValues["id"]?.ToString();
+
+
+    var filePath = Path.Combine(app.Environment.ContentRootPath, "images.json");
+
+    if (!File.Exists(filePath))
+    {
+        context.Response.StatusCode = StatusCodes.Status404NotFound;
+        await context.Response.WriteAsync("Json Object not found");
+        return;
+    }
+
+
+    var jsonData = await File.ReadAllTextAsync(filePath);
+
+    var picData = JsonSerializer.Deserialize<pictureData>(jsonData);
+    var imagePath = $"{uniqueId}{Path.GetExtension(picData.Image)}";
+
+    var htmlContent = $@"
+        <!DOCTYPE html>
+        <html lang='en'>
+        <head>
+            <meta charset='UTF-8'>
+            <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+            <title>Picture</title>
+        </head>
+        <body>
+            <h1>{picData.Name}</h1>
+            <img src='../data/{imagePath}' alt='{picData.Name}'>
+        </body>
+        </html>";
+
+    context.Response.ContentType = "text/html";
+
+
+    await context.Response.WriteAsync(htmlContent);
+});
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
 
 app.Run();
---
+++ imageUploaderAPI/wwwroot/index.js
@@ -0,0 +1,38 @@
+﻿const imgInput = document.querySelector("#imgExtension");
+const nameInput = document.querySelector("#imageName");
+const form = document.querySelector("form");
+function validate() {
+    if (nameInput.value === "") {
+        document.querySelector(".nameValidation").classList.remove("d-none");
+        return false;
+    } else {
+        document.querySelector(".nameValidation").classList.add("d-none");
+    }
+    if (imgInput.value === "" || !imgInput.value.match(/\.(jpeg|png)$/)) {
+        document.querySelector(".imgValidation").classList.remove("d-none");
+        return false;
+    } else {
+        document.querySelector(".imgValidation").classList.add("d-none");
+    }
+    return true;
+}
+
+imgInput.addEventListener("change", validate);
+nameInput.addEventListener("change", validate);
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    // if (validate()) {
+        const formData = new FormData();
+        formData.append("img", imgInput.files[0]);
+        formData.append("name", nameInput.value);
+        axios.post("/api", formData).then((res) => {
+            if(res.status === 200) {
+                window.location.href = `/picture/${res.data}`;
+            }
+        })
+        .catch((err) => {
+            console.log("Error: ", err.response.data);
+        });
+    // }
+});
