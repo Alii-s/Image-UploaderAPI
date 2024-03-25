25624830f76ac16a0308e7f23d9b2339bf9ccef0
Alii-s <119129254+Alii-s@users.noreply.github.com>
2024-03-25 17:50:38 +0200

    fixing deployment issues

--- ImageUploaderAPI/Program.cs
+++ ImageUploaderAPI/Program.cs
@@ -42,7 +42,7 @@ app.MapPost("/api", async (HttpContext context) =>
     };
     var json = JsonSerializer.Serialize(data);
     var filePath = Path.Combine(app.Environment.ContentRootPath,"images.json");
-    var imageFilePath = Path.Combine(app.Environment.ContentRootPath, "data", $"{data.id}{Path.GetExtension(data.Image)}");
+    var imageFilePath = Path.Combine("data", $"{data.id}{Path.GetExtension(data.Image)}");
     using (var stream = new FileStream(imageFilePath, FileMode.Create))
     {
         await file.CopyToAsync(stream);
