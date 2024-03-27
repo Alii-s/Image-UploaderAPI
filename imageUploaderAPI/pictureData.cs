57fa4c600db7e29d9b2b41cacb45becb6854d83f
Alii-s <119129254+Alii-s@users.noreply.github.com>
2024-03-27 12:08:59 +0200

    Fixing Directories #2

---
+++ imageUploaderAPI/pictureData.cs
@@ -0,0 +1,9 @@
+﻿namespace imageUploaderAPI
+{
+    public class pictureData
+    {
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public string id { get; set; }
+    }
+}
